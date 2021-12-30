1dd41713455bdefb64ba31c41dcea900ee9dc941
milon27 <mdjahidulislammilon@gmail.com>
2021-12-30 22:06:19 +0600

    prod version

--- models/config.js
+++ models/config.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql')
 require('dotenv').config();
 
-const host = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.HOST2}` : `${process.env.HOST}`;//private field
-const user = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.USER2}` : `${process.env.USER}`;//private field
-const pass = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.PASS2}` : `${process.env.PASS}`;//private field
-const database = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.DB2}` : `${process.env.DB}`;//private field
+const host = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.HOST2}` : `${process.env.P_HOST}`;//private field
+const user = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.USER2}` : `${process.env.P_USER}`;//private field
+const pass = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.PASS2}` : `${process.env.P_PASS}`;//private field
+const database = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.DB2}` : `${process.env.P_DB}`;//private field
+const port = (`${process.env.NODE_ENV}` === "dev") ? `${process.env.PORT2}` : `${process.env.P_PORT}`;//private field
+
+
 
 console.log(host, user, pass, database);
 
@@ -13,6 +16,7 @@ const pool = mysql.createPool({
     connectionLimit: 10,
     host: host,
     user: user,
+    port: port,
     password: pass,
     database: database,
     timezone: 'gmt+6'  //<-here this line was missing 'utc'
